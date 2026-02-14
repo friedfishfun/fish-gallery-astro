44d1a9f71916a9f797518530c694ae9373de7634
friedfishfun <temtemtaro@yahoo.co.jp>
2026-02-14 12:12:47 +0900

    Update site URL and add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://astro-simple-blog-omega.vercel.app/",
+  site: "https://astro-simple-blog.vercel.app",
   integrations: [tailwind()],
+  adapter: vercel(),
 });
